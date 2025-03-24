a710c9738eeff1365d90614c2911b223ac529779
Konstantin <69057532+archef2000@users.noreply.github.com>
2025-03-24 08:51:05 +0100

    add longer time for reset of JTAG (#24)

--- src/web/utils.ts
+++ src/web/utils.ts
@@ -20,13 +20,15 @@ import { FileType, StatusBarAlignment, Uri, window, workspace, FileSystemError, 
 import { FlashSectionMessage, PartitionInfo } from "./webserial";
 import { Transport, UsbJtagSerialReset } from "esptool-js";
 
+const USB_JTAG_SERIAL_PID = 0x1001;
+
 export const errorNotificationMessage =
   "Build file not found. Make sure to build your ESP-IDF project first and if 'idf.buildPath' is defined, that is correctly set.";
 // https://issues.chromium.org/issues/40137537
 const webUsbPolyfillClaimError = "Failed to execute 'claimInterface' on 'USBDevice': Unable to claim interface.";
 
 const encoder = new TextEncoder();
-export const stringToUInt8Array = function(textString: string) {return encoder.encode(textString);};
+export const stringToUInt8Array = function (textString: string) { return encoder.encode(textString); };
 
 export function uInt8ArrayToString(fileBuffer: Uint8Array) {
   let fileBufferString = "";
@@ -46,6 +48,9 @@ export async function universalReset(transport: Transport) {
     await transport.setDTR(true);
   } else { // WebUSB polyfill
     new UsbJtagSerialReset(transport).reset();
+    if (transport.getPid() === USB_JTAG_SERIAL_PID) {
+      await sleep(100);
+    }
     await sleep(100);
     // can also use SerialReset twice, but then the chip gets reset 1.5 times
     await transport.setRTS(false);
